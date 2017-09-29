6993af79aaed9a977544e2546813f8f78df5cd5e
Ferkat Jawdat <ferkat0321@gmail.com>
2017-09-29 15:42:06 -0400

    Rverted back to older version

--- src/main/java/com/aps/testing/util/CucumberRunner.java
+++ src/main/java/com/aps/testing/util/CucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = { "pretty", "json:target/" }, features = { "src/main/java/com/aps/testing/cucumber/features" })
+@CucumberOptions(format = { "pretty", "json:target/" }, features = { "src/main/java/com/aps/testing/features" })
 public class CucumberRunner {
 
 }
